f41c99355dfc1335fe1608f39e7af3fe030f90c3
mantis28 <59893472+mantis28@users.noreply.github.com>
2023-08-06 17:01:35 -0400

    Update nfty.sol

--- nfty.sol
+++ nfty.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+// nft contract
+contract nfty is ERC721URIStorage, Ownable {
+
+    constructor() ERC721("NFTY", "nfty") {}
+
+    function mint(
+
+        address to, 
+        uint256 tokenId, 
+        string calldata _uri
+        ) external onlyOwner {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, _uri);
+
+    }
+}
